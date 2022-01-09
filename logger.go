beff49b108bee2a2f80a93ea2a7ed1fda75ffc43
evenstephenr <github@ramirs.com>
2022-01-09 15:22:29 -0500

    refactor for extensibility

--- logger.go
+++ logger.go
@@ -8,20 +8,34 @@ import (
 var debug = flag.Bool("debug", false, "enable debug logs")
 var debugShort = flag.Bool("d", false, "enable debug logs")
 
-func Error(message string, context interface{}) {
-	fmt.Println("ERROR:", message, context)
+func Error(message string) string {
+	return ("ERROR: " + message)
 }
 
-func Info(message string, context interface{}) {
-	fmt.Println("INFO:", message, context)
+func Info(message string) string {
+	return ("INFO: " + message)
 }
 
-func Warn(message string, context interface{}) {
-	fmt.Println("WARN:", message, context)
+func Warn(message string) string {
+	return ("WARN: " + message)
 }
 
-func Debug(message string, context interface{}) {
+func Debug(message string) string {
 	if (*debug) || (*debugShort) {
-		fmt.Println("DEBUG:", message, context)	
+		return ("DEBUG: " + message)
+	}
+
+	return ""
+}
+
+func ReportContext(s string, context interface{}) {
+	if (len(s) > 0) {
+		fmt.Println(s, context)
+	}
+}
+
+func Report(s string) {
+	if (len(s) > 0) {
+		fmt.Println(s)
 	}
 }
