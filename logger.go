ed3661f6fc0bd4f78b59a473de97f777fe3b7c1f
evenstephenr <github@ramirs.com>
2022-01-09 13:37:16 -0500

    add Warn

--- logger.go
+++ logger.go
@@ -3,9 +3,13 @@ package logger
 import "fmt"
 
 func Error(message string, context interface{}) {
-	fmt.Println("ERROR: ", message, context)
+	fmt.Println("ERROR:", message, context)
 }
 
 func Info(message string, context interface{}) {
-	fmt.Println("INFO: ", message, context)
+	fmt.Println("INFO:", message, context)
+}
+
+func Warn(message string, context interface{}) {
+	fmt.Println("WARN:", message, context)
 }
